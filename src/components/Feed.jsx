8f19c09fd36cf071ba989bc4994dd646be610ebd
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-24 16:09:46 +0900

    adding title on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,6 +21,16 @@ const Feed = () => {
           Copyright 2022 JK Youtube
         </Typography>
       </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+      </Box>
     </Stack>
   );
 };
