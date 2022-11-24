d5cb72d75057cb0b47764ea46ccb070591dec4fe
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-24 16:30:17 +0900

    setting up API connection

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import {Sidebar, Videos} from "./";
 
 const Feed = () => {
   return (
@@ -30,6 +30,7 @@ const Feed = () => {
         >
           New <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
